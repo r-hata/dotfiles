cde668759cb6983a045dc11a3c62059aa5f11be6
Ryosuke Hata <rh.ts124@gmail.com>
2018-12-08 19:24:35 +0900

    Add .tmux.conf

--- setup.sh
+++ setup.sh
@@ -5,3 +5,4 @@ REPOSITORY_DIR=$(cd $(dirname $0); pwd)
 ln -s $REPOSITORY_DIR/gitconfig $HOME/.gitconfig
 ln -s $REPOSITORY_DIR/vimrc $HOME/.vimrc
 ln -s $REPOSITORY_DIR/gvimrc $HOME/.gvimrc
+ln -s $REPOSITORY_DIR/tmux.conf $HOME/.tmux.conf
