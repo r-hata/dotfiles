822599fa1b40df5c13c43d83c13f881bdfe1176c
Ryosuke Hata <rysk.hata@gmail.com>
2019-02-02 22:48:13 +0900

    Rename files

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 
 REPOSITORY_DIR=$(cd $(dirname $0); pwd)
 
-ln -s $REPOSITORY_DIR/gitconfig $HOME/.gitconfig
-ln -s $REPOSITORY_DIR/vimrc $HOME/.vimrc
-ln -s $REPOSITORY_DIR/gvimrc $HOME/.gvimrc
-ln -s $REPOSITORY_DIR/tmux.conf $HOME/.tmux.conf
+ln -s $REPOSITORY_DIR/.gitconfig $HOME/.gitconfig
+ln -s $REPOSITORY_DIR/.vimrc $HOME/.vimrc
+ln -s $REPOSITORY_DIR/.gvimrc $HOME/.gvimrc
+ln -s $REPOSITORY_DIR/.tmux.conf $HOME/.tmux.conf
