d561fbb7af0924690cafc0f186b855da3df220ee
Ryosuke Hata <rysk.hata@gmail.com>
2019-06-08 22:40:24 +0900

    Add setup neovim config

--- setup.sh
+++ setup.sh
@@ -6,3 +6,9 @@ ln -s $REPOSITORY_DIR/.gitconfig $HOME/.gitconfig
 ln -s $REPOSITORY_DIR/.vimrc $HOME/.vimrc
 ln -s $REPOSITORY_DIR/.gvimrc $HOME/.gvimrc
 ln -s $REPOSITORY_DIR/.tmux.conf $HOME/.tmux.conf
+
+NVIM_CONFIG_DIR=$HOME/.config/nvim
+if [ ! -d ${NVIM_CONFIG_DIR} ]; then
+    mkdir -p $NVIM_CONFIG_DIR
+fi
+ln -s $REPOSITORY_DIR/.vimrc $NVIM_CONFIG_DIR/init.vim
