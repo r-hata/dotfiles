76f5413fd70d7bc76a419e81ddaf70344aeec25c
Ryosuke Hata <rysk.hata@gmail.com>
2019-06-11 07:03:19 +0900

    Make idempotence to setup.sh

--- setup.sh
+++ setup.sh
@@ -2,13 +2,13 @@
 
 REPOSITORY_DIR=$(cd $(dirname $0); pwd)
 
-ln -s $REPOSITORY_DIR/.gitconfig $HOME/.gitconfig
-ln -s $REPOSITORY_DIR/.vimrc $HOME/.vimrc
-ln -s $REPOSITORY_DIR/.gvimrc $HOME/.gvimrc
-ln -s $REPOSITORY_DIR/.tmux.conf $HOME/.tmux.conf
+ln -sf $REPOSITORY_DIR/.gitconfig $HOME/.gitconfig
+ln -sf $REPOSITORY_DIR/.vimrc $HOME/.vimrc
+ln -sf $REPOSITORY_DIR/.gvimrc $HOME/.gvimrc
+ln -sf $REPOSITORY_DIR/.tmux.conf $HOME/.tmux.conf
 
 NVIM_CONFIG_DIR=$HOME/.config/nvim
 if [ ! -d ${NVIM_CONFIG_DIR} ]; then
     mkdir -p $NVIM_CONFIG_DIR
 fi
-ln -s $REPOSITORY_DIR/.vimrc $NVIM_CONFIG_DIR/init.vim
+ln -sf $REPOSITORY_DIR/.vimrc $NVIM_CONFIG_DIR/init.vim
