b64bb00acb513265a255aa304bbf8814f6d6f3b0
Ryosuke Hata <tech@r-hata.net>
2024-11-26 19:27:15 +0900

    Fish: Add Homebrew setting

--- fish/config.fish
+++ fish/config.fish
@@ -18,6 +18,8 @@ if status is-interactive
   # Initialize linuxbrew
   if test -d "/home/linuxbrew"
     eval (/home/linuxbrew/.linuxbrew/bin/brew shellenv)
+  else if test -d "/opt/homebrew"
+    eval (/opt/homebrew/bin/brew shellenv)
   end
 
   # Initialize golang
