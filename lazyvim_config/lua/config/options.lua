9e933d483ec0fef9c0b19a0d14a3d89f9cff3c3b
Ryosuke Hata <tech@r-hata.net>
2026-05-25 10:14:41 +0900

    vim: change options

--- lazyvim_config/lua/config/options.lua
+++ lazyvim_config/lua/config/options.lua
@@ -1,3 +1,13 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+vim.g.snacks_animate = false
+vim.opt.mouse = ""
+vim.keymap.set("i", "jj", "<Esc>")
+vim.g.python3_host_prog = (function()
+  local python3_path = os.getenv("HOME") .. "/.venv/bin/python3"
+  if vim.fn.filereadable(python3_path) == 0 then
+    python3_path = ""
+  end
+  return python3_path
+end)()
