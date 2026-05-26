cfe8d14811b1dd77fffeccfd78cd846598fc9c59
Ryosuke Hata <tech@r-hata.net>
2026-05-26 22:01:38 +0900

    apply formatter to setup.sh

--- setup.sh
+++ setup.sh
@@ -4,8 +4,8 @@ set -eo pipefail
 LN_OPTION="-s${OPTION}"
 
 REPOSITORY_DIR=$(
-	cd "$(dirname "$0")"
-	pwd
+  cd "$(dirname "$0")"
+  pwd
 )
 
 ln "$LN_OPTION" "$REPOSITORY_DIR"/.gitconfig "$HOME"/.gitconfig
@@ -13,21 +13,21 @@ ln "$LN_OPTION" "$REPOSITORY_DIR"/.gitconfig "$HOME"/.gitconfig
 DOT_CONFIG_DIR=$HOME/.config
 LAZYGIT_DIR=$DOT_CONFIG_DIR/lazygit
 if [ ! -d "${LAZYGIT_DIR}" ]; then
-	mkdir -p "$LAZYGIT_DIR"
+  mkdir -p "$LAZYGIT_DIR"
 fi
 ln "$LN_OPTION" "$REPOSITORY_DIR"/lazygit/config.yml "$LAZYGIT_DIR"/config.yml
 FISH_DIR=$DOT_CONFIG_DIR/fish
 if [ ! -d "${FISH_DIR}" ]; then
-	mkdir -p "$FISH_DIR"
+  mkdir -p "$FISH_DIR"
 fi
 ln "$LN_OPTION" "$REPOSITORY_DIR"/fish/config.fish "$FISH_DIR"
 
 NVIM_DIR=$DOT_CONFIG_DIR/nvim
 if [ -d "${NVIM_DIR}" ]; then
-	if [ "$OPTION" != "f" ]; then
-		read -r -p "overwrite '${NVIM_DIR}'? (y/N): " yn
-		case "$yn" in [yY]*) ;; *) exit ;; esac
-	fi
-	rm -rf "${NVIM_DIR}"
+  if [ "$OPTION" != "f" ]; then
+    read -r -p "overwrite '${NVIM_DIR}'? (y/N): " yn
+    case "$yn" in [yY]*) ;; *) exit ;; esac
+  fi
+  rm -rf "${NVIM_DIR}"
 fi
 ln "$LN_OPTION" "$REPOSITORY_DIR"/lazyvim_config "$NVIM_DIR"
