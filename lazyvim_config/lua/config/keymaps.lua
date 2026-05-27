6a381a901891fc828139c4293163359f3dbfd53d
Ryosuke Hata <tech@r-hata.net>
2026-05-27 22:29:17 +0900

    vim: add keymaps

--- lazyvim_config/lua/config/keymaps.lua
+++ lazyvim_config/lua/config/keymaps.lua
@@ -2,3 +2,4 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 vim.keymap.set("i", "jj", "<Esc>")
+vim.keymap.set("i", "<C-l>", "<Del>")
